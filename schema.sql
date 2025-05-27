c742044b5865d92270a76e1be32492c325858567
Sahil Gupta <162191477+SahilGupta99@users.noreply.github.com>
2025-05-27 23:18:33 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `product_management` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `product_management`;
--- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
 -- Host: localhost    Database: product_management
 -- ------------------------------------------------------
--- Server version	8.0.40
+-- Server version	8.0.39
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,32 @@ USE `product_management`;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `admin_user`
+--
+
+DROP TABLE IF EXISTS `admin_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `admin_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(80) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `admin_user`
+--
+
+LOCK TABLES `admin_user` WRITE;
+/*!40000 ALTER TABLE `admin_user` DISABLE KEYS */;
+INSERT INTO `admin_user` VALUES (1,'Lucifer','Lucifer');
+/*!40000 ALTER TABLE `admin_user` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `category`
@@ -31,7 +57,7 @@ CREATE TABLE `category` (
   `image_url` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +66,7 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
-INSERT INTO `category` VALUES (21,'Prefabricated House','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals. We are proficient to offer an extensive collection of portable farm houses in vary designs to our respected customers. Consumers can avail this farm house from us according to their specifications at the industry leading price.','/static/uploads/categories/category_1745398081_virtualshowroom.png'),(22,'Portable House','AMS Enterprises extensive range of Portable House is manufactured using superior quality panels and other rawmaterials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUFpanels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliantprofessionals. We are proficient to offer an extensive collection of portable farm houses in vary designs to our respected customers.Consumers can avail this farm house from us according to their specifications at the','/static/uploads/categories/category_1745398105_karsten-wurth-7BjhtdogU3A-unsplash.jpg'),(23,'Portable Cabin AMS Group','Service Provider of a wide range of services which include ms porta cabin, 15 x10 feet wooden portable cabin, 10x10 feet ms porta cabin, porta cabin manufacturers in delhi, porta cabin in chandigarh and porta cabin in dehradun.','/static/uploads/categories/category_1745398117_zoltan-tasi-sJGvoX_eVhw-unsplash.jpg');
+INSERT INTO `category` VALUES (24,'A frame','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals.','/static/uploads/category-images/category_1747199685_1.jpg.jpg'),(25,'wooden house','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals.','/static/uploads/category-images/category_1747199705_2.jpg.jpg'),(26,'double storey','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals.','/static/uploads/category-images/category_1747199788_3.jpg.jpg'),(27,'Prefabricated Luxury House','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals.','/static/uploads/category-images/category_1747199828_4.jpg.jpg');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -95,7 +121,7 @@ CREATE TABLE `product` (
   PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`),
   CONSTRAINT `product_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,7 +130,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (7,21,'Wooden Frame Houses','This is a farm houses ',1400.00,'\"{\\\"Ground Floor Dimensions\\\": \\\"Ground Floor\\\", \\\"Ground floor Area\\\": \\\"1000 Sq ft\\\", \\\"Material\\\": \\\"Steel\\\", \\\"Minimum order quantity\\\": \\\"1000 sq ft\\\", \\\"Place of Origin\\\": \\\"Pan India\\\", \\\"size\\\": \\\"500 Sq ft\\\"}\"','\"/static/uploads/A-1.jpg,/static/uploads/A-2.jpg,/static/uploads/A-3.jpg,/static/uploads/A-4.jpg,/static/uploads/A-5.jpg\"',''),(8,21,'Wooden Modular Prefabricated Cottage','Prefabricated houses, or prefab houses, are homes constructed using factory-made components or modules that are then assembled on-site. These components, which can include walls, floors, and roofing,',1400.00,'{\"size\": \"500 Sq ft\", \"Material\": \"Steel\", \"Place of Origin\": \"Pan India\", \"Ground floor Area\": \"1000 Sq ft\", \"Minimum order quantity\": \"1000 sq ft\", \"Ground Floor Dimensions\": \"Ground Floor\"}','\"/static/uploads/1.jpg,/static/uploads/2.jpg,/static/uploads/3.jpeg,/static/uploads/4.jpeg,/static/uploads/5.jpg\"',''),(9,22,'Rectangular Portable Houses','Prefabricated houses, or prefab houses, are homes constructed using factory-made components or modules that are then assembled on-site. These components, which can include walls, floors, and roofing,',1400.00,'{\"Color\": \"grey\", \"Shape\": \"Rectangular\", \"Material\": \"Mild Steel\", \"Build Type\": \"Panel Build\", \"Window Type\": \"Aluminium\", \"Usage/Application\": \"House\"}','\"/static/uploads/6.jpg,/static/uploads/7.jpg,/static/uploads/8.jpg,/static/uploads/9.jpg\"',''),(10,23,'Rectangular Portable Houses','Prefabricated houses, or prefab houses, are homes constructed using factory-made components or modules that are then assembled on-site. These components, which can include walls, floors, and roofing,',1400.00,'{\"Color\": \"grey\", \"Material\": \"Mild Steel\", \"Is It Portable\": \"No\", \"Country of Origin\": \"Made in India\", \"Is It Customized\\t\": \"Yes\", \"Minimum order quantity\": \"1000 sq ft\"}','\"/static/uploads/6.jpg,/static/uploads/7.jpg,/static/uploads/8.jpg,/static/uploads/9.jpg\"',''),(11,21,'new product1','this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description this is description ',3213.00,'{\"none\": \"none\", \"Color\": \"none\", \"Material\": \"none\", \"Warranty\": \"none\"}','\"/static/uploads/michael-mouritz-WXX_DhjlmD4-unsplash.jpg,/static/uploads/pexels-eberhard-grossgasteiger-2098427.jpg,/static/uploads/pexels-lachlan-ross-6510344.jpg,/static/uploads/pexels-guilherme-rossi-1755683.jpg,/static/uploads/pexels-aleksandar-pasaric-2603464.jpg\"','/static/uploads/productvideo.mp4'),(12,22,'sahil','this is the description',654.00,'{\"none\": \"none\", \"Color\": \"none\", \"Material\": \"none\", \"Warranty\": \"none\"}','\"/static/uploads/zoltan-tasi-sJGvoX_eVhw-unsplash.jpg,/static/uploads/jordon-conner-VIcTzkzNZR8-unsplash.jpg,/static/uploads/karsten-wurth-7BjhtdogU3A-unsplash.jpg,/static/uploads/michael-mouritz-WXX_DhjlmD4-unsplash.jpg,/static/uploads/pexels-eberhard-grossgasteiger-2098427.jpg\"','/static/uploads/istockphoto-1967534184-640_adpp_is.mp4');
+INSERT INTO `product` VALUES (17,24,'wooden A frame','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals. ',4444.00,'\"{\\\"Color\\\": \\\"brown\\\", \\\"Material\\\": \\\"wooden\\\", \\\"Warranty\\\": \\\"10\\\"}\"','\"/static/uploads/products/product_1747201841_3.jpg.jpg,/static/uploads/products/product_1747201841_4.jpg.jpg,/static/uploads/products/product_1747201841_5.jpg.jpg\"','/static/uploads/videos/video_1747201841_istockphoto-1967534184-640_adpp_is.mp4'),(18,25,'wooden house prefab','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals. ',4444.00,'{\"Color\": \"brown\", \"Material\": \"wooden\", \"Warranty\": \"10\"}','\"/static/uploads/products/product_1747201858_1.jpg.jpg,/static/uploads/products/product_1747201858_2.jpg.jpg,/static/uploads/products/product_1747201858_3.jpg.jpg,/static/uploads/products/product_1747201858_4.jpg.jpg,/static/uploads/products/product_1747201858_5.jpg.jpg\"','/static/uploads/videos/video_1747201858_istockphoto-1967534184-640_adpp_is.mp4'),(19,26,'premium house ','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals. ',4444.00,'{\"Color\": \"brown\", \"Material\": \"wooden\", \"Warranty\": \"10\"}','\"/static/uploads/products/product_1747201872_1.jpg.jpg,/static/uploads/products/product_1747201872_2.jpg.jpg,/static/uploads/products/product_1747201872_3.jpg.jpg,/static/uploads/products/product_1747201872_4.jpg.jpg,/static/uploads/products/product_1747201872_5.jpg.jpg\"','/static/uploads/videos/video_1747201872_istockphoto-1967534184-640_adpp_is.mp4'),(20,27,'prefab house','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals. ',4444.00,'{\"Color\": \"brown\", \"Material\": \"wooden\", \"Warranty\": \"10\"}','\"/static/uploads/products/product_1747201888_1.jpg.jpg,/static/uploads/products/product_1747201888_2.jpg.jpg,/static/uploads/products/product_1747201888_3.jpg.jpg,/static/uploads/products/product_1747201888_4.jpg.jpg,/static/uploads/products/product_1747201888_5.jpg.jpg\"','/static/uploads/videos/video_1747201888_istockphoto-1967534184-640_adpp_is.mp4'),(21,24,'house2','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals. ',4444.00,'{\"Color\": \"brown\", \"Material\": \"wooden\", \"Warranty\": \"10\"}','\"/static/uploads/products/product_1747201902_1.jpg.jpg,/static/uploads/products/product_1747201902_2.jpg.jpg,/static/uploads/products/product_1747201902_3.jpg.jpg,/static/uploads/products/product_1747201902_4.jpg.jpg,/static/uploads/products/product_1747201902_5.jpg.jpg\"','/static/uploads/videos/video_1747201902_istockphoto-1967534184-640_adpp_is.mp4'),(22,24,'house3','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals. ',4444.00,'{\"Color\": \"brown\", \"Material\": \"wooden\", \"Warranty\": \"10\"}','\"/static/uploads/products/product_1747201907_1.jpg.jpg,/static/uploads/products/product_1747201907_2.jpg.jpg,/static/uploads/products/product_1747201907_3.jpg.jpg,/static/uploads/products/product_1747201907_4.jpg.jpg,/static/uploads/products/product_1747201907_5.jpg.jpg\"','/static/uploads/videos/video_1747201907_istockphoto-1967534184-640_adpp_is.mp4'),(23,24,'house4','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals. ',4444.00,'{\"Color\": \"brown\", \"Material\": \"wooden\", \"Warranty\": \"10\"}','\"/static/uploads/products/product_1747201912_1.jpg.jpg,/static/uploads/products/product_1747201912_2.jpg.jpg,/static/uploads/products/product_1747201912_3.jpg.jpg,/static/uploads/products/product_1747201912_4.jpg.jpg,/static/uploads/products/product_1747201912_5.jpg.jpg\"','/static/uploads/videos/video_1747201912_istockphoto-1967534184-640_adpp_is.mp4'),(24,24,'house5','AMS Enterprises extensive range of Wooden House is manufactured using superior quality panels and other raw materials that ensure its robustness and long life. Farm houses are prepared with high grade steel, CFB, EPS, and PUF panels that are procured from our trusted and consistent merchant due to the assiduous efforts of our brilliant professionals. ',4444.00,'{\"Color\": \"brown\", \"Material\": \"wooden\", \"Warranty\": \"10\"}','\"/static/uploads/products/product_1747201918_1.jpg.jpg,/static/uploads/products/product_1747201918_2.jpg.jpg,/static/uploads/products/product_1747201918_3.jpg.jpg,/static/uploads/products/product_1747201918_4.jpg.jpg,/static/uploads/products/product_1747201918_5.jpg.jpg\"','/static/uploads/videos/video_1747201918_istockphoto-1967534184-640_adpp_is.mp4');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -117,4 +143,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-09 16:23:35
+-- Dump completed on 2025-05-27 22:56:35
