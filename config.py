bce367ed86f43dfa2c7625955ea18b7b7ee7fe43
SahilGupta99 <sahil.sg9971@gmail.com>
2025-05-27 21:50:12 +0530

    Initial commit for AMS Prefab Flask app

--- config.py
+++ config.py
@@ -1,16 +1,10 @@
 import os
 
 class Config:
-    # SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:root@localhost/product_management"
-    # SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://avnadmin:AVNS_pZzZA0U361XyCoY4kIG@product-management-product-management.l.aivencloud.com:18798/defaultdb?ssl_mode=REQUIRED"
-    SQLALCHEMY_DATABASE_URI = (
-    "mysql+mysqlconnector://avnadmin:AVNS_6Y_ELomxkTzGtorz1p5@"
-    "prefab-ams-prefab.l.aivencloud.com:22480/defaultdb"
-    "?ssl_ca=/Product-Management-final/ca.pem"
-)
-
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:root@localhost/product_management"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = "static/uploads"
     ALLOWED_IMAGE_EXTENSIONS = {"jpg", "jpeg", "png", "gif"}
     ALLOWED_VIDEO_EXTENSIONS = {"mp4", "mov", "avi"}
-"Product-Management-final\ca.pem"
+
+
