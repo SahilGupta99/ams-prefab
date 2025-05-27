3df4f4017db018cec1948c60aaeaf47b3b8a8615
Sahil Gupta <162191477+SahilGupta99@users.noreply.github.com>
2025-05-27 23:20:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:root@localhost/product_management"
+    # SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://root:root@localhost/product_management"
+    SQLALCHEMY_DATABASE_URI = (
+        "mysql+mysqlconnector://avnadmin:AVNS_6Y_ELomxkTzGtorz1p5@"
+        "prefab-ams-prefab.l.aivencloud.com:22480/defaultdb"
+        "?ssl_ca=/Product-Management-final/ca.pem"
+        )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = "static/uploads"
     ALLOWED_IMAGE_EXTENSIONS = {"jpg", "jpeg", "png", "gif"}
